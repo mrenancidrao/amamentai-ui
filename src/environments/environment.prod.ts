912d9b92d6e390f08a8e76dad15a527d0bea4bdd
renan <mrenancidrao@gmail.com>
2018-05-17 10:53:39 -0300

    sadsd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://amamentai-api.herokuapp.com'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://amamentai-api.herokuapp.com'
+  // apiUrl: 'http://localhost:8080'
 };
